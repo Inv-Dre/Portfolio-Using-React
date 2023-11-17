6dc0afe07e986e0bf306800edbeb5f50518d10fc
Dre Tay <andretaylor681@gmail.com>
2023-11-17 13:08:23 -0500

    changed the class name on the navbar component to nav-link-active and added css so the active link could be seen

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,25 +10,25 @@ function Navigation () {
                 <li>
                     <Link
                     to='/About'
-                    className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
+                    className={currentPage === '/About' ? 'nav-link-active' : 'nav-link'}
                     >About Me</Link>   
                 </li>
                 <li>
                     <Link
                     to='/Portfolio'
-                    className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+                    className={currentPage === '/Portfolio' ? 'nav-link-active' : 'nav-link'}
                     >Portfolio</Link>
                 </li>
                 <li>
                 <Link
                     to='/Contact'
-                    className={currentPage === '/Contact-me' ? 'nav-link active' : 'nav-link'}
+                    className={currentPage === '/Contact' ? 'nav-link-active' : 'nav-link'}
                     >Contact Me</Link>
                 </li>
                 <li>
                 <Link
                     to='/Resume'
-                    className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+                    className={currentPage === '/Resume' ? 'nav-link-active' : 'nav-link'}
                     >Resume</Link>
                 </li>
             </ul>
