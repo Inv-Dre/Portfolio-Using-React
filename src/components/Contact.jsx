304a751e3fa86d6aef5ba1b659586c66d7e32985
Dre Tay <andretaylor681@gmail.com>
2023-11-20 17:00:45 -0500

    edited my contact me page added labels and buttons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,45 @@
+function Contact() {
+  return (
+    <div className="contact-me">
+      <h3>Contact Me</h3>
 
-function Contact (){
-    return(
-        <div className="contact-me">
-        <h3> 
-            Contact Me
-        </h3>
-        <form>
-           <input type = 'text' placeholder = 'type your name' name = 'name'  ></input>
-        </form>
-        <form>  
-            <input type = 'text' placeholder = 'type your email' name = 'email'  ></input>
-        </form> 
-        <form>
-            <input type = 'text' placeholder = 'short message' name = 'message' ></input>
-        </form>
+      <form className="was-validated">
+        <div>
+          <label for="name">Name:</label>
+          <input 
+            type="text"
+            placeholder="Your name"
+            name="name"
+            required
+          ></input>
         </div>
-
-    )
+        <div>
+          <label for="email">Email:</label>
+          <input
+            type="email"
+            placeholder="Your email"
+            name="email"
+            required
+          ></input>
+        </div>
+        <div>
+          <label for="message">Message:</label>
+          <textarea
+            type="message"
+            placeholder="Short message"
+            name="message"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" type="reset">Reset</button>
+        <button className="btn btn-success" type="submit">Submit</button>
+      </form>
+      <div>
+        <a target="_blank" href="mailto:AndreTaylor681@gmail.com">
+          Click to email me
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
